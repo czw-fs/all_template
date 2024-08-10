fc05850da5df9a17bd850ec4dc2ed8c97c774ad3
chenziwen <chenziwen0413@163.com>
2024-08-10 15:58:13 +0800

    init

---
+++ SpringSecurityBase/src/main/java/com/example/springsecuritybase/SpringSecurityBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritybase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBaseApplication.class, args);
+    }
+
+}
