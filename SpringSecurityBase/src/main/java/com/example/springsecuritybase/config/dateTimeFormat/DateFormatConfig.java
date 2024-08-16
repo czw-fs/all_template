2b080513f8286edb0050d8c677c1b25fa3c42065
chenziwen <chenziwen0413@163.com>
2024-08-16 11:20:22 +0800

    全局日期格式化，mp分页

--- SpringSecurityBase/src/main/java/com/example/springsecuritybase/config/dateTimeFormat/DateFormatConfig.java
+++ SpringSecurityBase/src/main/java/com/example/springsecuritybase/config/dateTimeFormat/DateFormatConfig.java
@@ -1,56 +1,58 @@
 package com.example.springsecuritybase.config.dateTimeFormat;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.TimeZone;
 
 /**
  * 全局日期格式化 如果某个字段不使用该格式
- * 依旧可以使用 @JsonFormat(locale = "zh", timezone = "GMT+8", pattern = "yyyy-MM-dd") 修改某个字段的格式化信息，且@JsonFormat优先级高于@JsonComponent配置的格式类型
+ * 依旧可以使用 @JsonFormat 和 @DataTimeFormat 覆盖全局配置
  */
 @JsonComponent
 @Configuration
 public class DateFormatConfig {
 
-    @Value("${spring.jackson.date-format}")
-    private String pattern;
+    @Value("${spring.mvc.format.date-time}")
+    private String dateTimePattern;
 
-    /**
-     * 类型全局时间格式化
-     */
-    @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilder() {
-        return builder -> {
-            TimeZone tz = TimeZone.getTimeZone("GMT+8");
-            DateFormat df = new SimpleDateFormat(pattern);
-            df.setTimeZone(tz);
-            builder.failOnEmptyBeans(false)
-                    .failOnUnknownProperties(false)
-                    .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                    .dateFormat(df);
-        };
-    }
+    @Value("${spring.mvc.format.date}")
+    private String datePattern;
 
-    /**
-     * 类型全局时间格式化
-     */
-    @Bean
-    public LocalDateTimeSerializer localDateTimeDeserializer() {
-        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern));
-    }
+    @Value("${spring.mvc.format.time}")
+    private String timePattern;
 
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
-        return builder -> builder.serializerByType(LocalDateTime.class, localDateTimeDeserializer());
+        return builder -> {
+            TimeZone tz = TimeZone.getTimeZone("GMT+8");
+
+            builder.failOnEmptyBeans(false)
+                    .failOnUnknownProperties(false)
+                    .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                    .timeZone(tz)
+                    .serializers(
+                            new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimePattern)),
+                            new LocalDateSerializer(DateTimeFormatter.ofPattern(datePattern)),
+                            new LocalTimeSerializer(DateTimeFormatter.ofPattern(timePattern))
+                    )
+                    .deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(dateTimePattern)))
+                    .deserializerByType(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(datePattern)))
+                    .deserializerByType(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(timePattern)));
+        };
     }
 }
