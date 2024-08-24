142f72c64081962c808ea6c5394212e5be31042a
chenziwen <1954635917@qq.com>
2024-08-24 14:51:53 +0800

    全局异常处理

--- SpringSecurityBase/src/main/java/com/example/springsecuritybase/exeception/GlobalWebExceptionHandle.java
+++ SpringSecurityBase/src/main/java/com/example/springsecuritybase/exeception/GlobalWebExceptionHandle.java
@@ -2,59 +2,13 @@ package com.example.springsecuritybase.exeception;
 
 import com.example.springsecuritybase.modules.common.model.Result;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindException;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-@Slf4j
 @RestControllerAdvice
 public class GlobalWebExceptionHandle {
-
-    private static final String BAD_REQUEST_MSG = "客户端请求参数错误";
-
-    // <1> 处理 form data方式调用接口校验失败抛出的异常
-    @ExceptionHandler(BindException.class)
-    public Result<List<String>> bindExceptionHandler(BindException e) {
-
-        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
-        List<String> collect = fieldErrors.stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.toList());
-
-        return Result.error(HttpStatus.BAD_REQUEST.value(), BAD_REQUEST_MSG, collect);
-    }
-
-    // <2> 处理 json 请求体调用接口校验失败抛出的异常
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Result<List<String>> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
-
-        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
-        List<String> collect = fieldErrors.stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.toList());
-
-        return Result.error(HttpStatus.BAD_REQUEST.value(), BAD_REQUEST_MSG, collect);
-    }
-
-
-    // <3> 处理单个参数校验失败抛出的异常
-    @ExceptionHandler(ConstraintViolationException.class)
-    public Result<List<String>> constraintViolationExceptionHandler(ConstraintViolationException e) {
-
-        Set<ConstraintViolation<?>> constraintViolations = e.getConstraintViolations();
-        List<String> collect = constraintViolations.stream().map(ConstraintViolation::getMessage).collect(Collectors.toList());
-
-        return Result.error(HttpStatus.BAD_REQUEST.value(), BAD_REQUEST_MSG, collect);
-    }
-
 
     /**
      * 匹配不到对应请求路径或获取请求参数格式错误
@@ -74,7 +28,6 @@ public class GlobalWebExceptionHandle {
     @ExceptionHandler(value = Exception.class)
     public Result<String> exceptionHandler(HttpServletResponse response, Exception e) {
         response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        log.error("服务器异常", e);
         return Result.error(500,"服务器异常" + e.getMessage());
     }
 
